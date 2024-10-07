97856ac19bd9e10becb5e59fb3366154dee4725b
KevinEncora <kevin.frias@encora.com>
2024-10-07 07:31:17 -0600

    First Commit - Added Leetcode Problem
    Added the first leetcode problem

---
+++ Problems/Number of Unequal Triplets in Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        int ans = 0, n = nums.size();
+
+        for (int i = 0; i < n - 2; i++){
+            for (int j = i + 1; j < n - 1; j++){
+                for (int k = j + 1; k < n; k++){
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] !=  nums[k]) 
+                        ans++;
+                }
+            }
+        }
+
+
+        return ans;
+    }
+};
